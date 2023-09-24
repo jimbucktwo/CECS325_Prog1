8b341475f6addb36b37278b74910fe74b79446a7
jimbucktwo <jimmytraann@gmail.com>
2023-09-24 03:34:10 -0700

    modified:   card.cpp

--- card.cpp
+++ card.cpp
@@ -1,16 +1,32 @@
 #include "card.h"
+using namespace std;
 
 class Card {
     public:
+        char rank;
+        char suit;
+        Card() {
+            rank = 'O';
+            suit = ' ';
+        }
 
-    Card() {
-        char rank = 'J';
-        char suit = ' ';
-    }
+        Card(char r, char s) {
+            char rank = r;
+            char suit = s;
+        }
 
-    Card(char r, char s) {
-        char rank = r;
-        char suit = s;
-    }
+        int getValue() {
+            if (Card().rank == 'J' || Card().rank == 'Q' || Card().rank == 'K' || Card().rank == 'T') 
+                return 10;
+            else if (Card().rank == 'A')
+                return 1;
+            else
+                return int(Card().rank);
 
+        }
 };
+
+int main() {
+    Card firstCard();
+    return 0;
+}
